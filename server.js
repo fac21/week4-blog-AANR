617688e34d8f9c3e4a1667e096bc05a7f7df271b
Amy Kuner <amykuner1@gmail.com>
2021-04-15 11:19:15 +0100

    heroku works!!!

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const homepageHandler = require("./routes/index.js");
 
 const PORT = process.env.PORT || 3000;
 
-
 server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
 
 server.use(staticHandler);
