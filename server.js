4c9292804d8b2392bbb1e0136839adddd87986c7
Amy Kuner <amykuner1@gmail.com>
2021-04-15 11:01:15 +0100

    changing port for heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const staticHandler = express.static("public");
 
 const homepageHandler = require("./routes/index.js");
 
-//const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
 
