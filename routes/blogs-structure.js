8b764dde2a7ddad62efc4b9da0501897f1523618
designaky <designaky@gmail.com>
2021-04-15 17:33:47 +0100

    Addig the delet test

--- routes/blogs-structure.js
+++ routes/blogs-structure.js
@@ -1,6 +1,8 @@
 const blogPosts = [
   { title: "pig", content: "hello", photo: "ok", id:"93023pig"},
-  { title: "nice pig", content: "hello ihave a nicepig", photo: "ok", id:"90923nice"}
+  { title: "nice pig", content: "hello ihave a nicepig", photo: "ok", id:"90923nice"},
+  { title: "Very nice pig", content: "My nice pig is nice", photo: "ok", id:"34235very"},
+  
 ]
 
 module.exports = blogPosts;
